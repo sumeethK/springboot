681baa2b8535b0f69f7dc5ce08126fac55768ee0
sumeethK <sumeethkanojia@gmail.com>
2017-12-17 23:10:10 +0530

    added junit

--- src/test/java/com/sumeeth/springboot/teenpatti/app/WinnerIdentifierImplTest.java
+++ src/test/java/com/sumeeth/springboot/teenpatti/app/WinnerIdentifierImplTest.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static com.sumeeth.springboot.teenpatti.Entity.Winner.WIN_TYPE.SAME_COLOR_HIGHER_WEIGHTAGE;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/teenpatti-context.xml"})
 public class WinnerIdentifierImplTest extends TestCase {
@@ -34,7 +36,8 @@ public class WinnerIdentifierImplTest extends TestCase {
         distributeCards();
         Optional<Winner> winner = winnerIdentifier.findWinner(playerList);
         assertTrue("There should be atleast one winner", winner.isPresent());
-        assertEquals("Winner should be Radhe.", "Radhe",
+        assertEquals("Wintype should be " + SAME_COLOR_HIGHER_WEIGHTAGE, SAME_COLOR_HIGHER_WEIGHTAGE, winner.get().getWinType());
+        assertEquals("Winner should be Sumeeth.", "Sumeeth",
                 winner.get().getPlayer().get().getName());
     }
 
@@ -46,9 +49,9 @@ public class WinnerIdentifierImplTest extends TestCase {
         playerList.get(0).setCardList(cardList1);
         playerList.get(0).setCardCount(3);
         List<Card> cardList2 = new ArrayList<>();
-        cardList2.add(new Card(2, Card.COLOR.BLACK, Card.TYPE.SPADE));
-        cardList2.add(new Card(4, Card.COLOR.RED, Card.TYPE.DIAMOND));
         cardList2.add(new Card(2, Card.COLOR.RED, Card.TYPE.DIAMOND));
+        cardList2.add(new Card(4, Card.COLOR.RED, Card.TYPE.DIAMOND));
+        cardList2.add(new Card(7, Card.COLOR.RED, Card.TYPE.DIAMOND));
         playerList.get(1).setCardList(cardList2);
         playerList.get(1).setCardCount(3);
         List<Card> cardList3 = new ArrayList<>();
